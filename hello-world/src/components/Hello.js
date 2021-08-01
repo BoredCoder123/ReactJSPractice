498e7016f4f68680d7bda2c2fbff83e21c9e39cb
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-02 00:59:10 +0530

    JSX notation

---
+++ hello-world/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    // return(          This is JSX code
+    //     <div className='dummyClass'>
+    //         <h1>Hello Ankit</h1>
+    //     </div>
+    // )
+    return React.createElement('div', {id: 'Hello', className: 'HelloC'}, React.createElement('h1', null, 'Hello Ankit'))      //This is without JSX
+}
+
+export default Hello
