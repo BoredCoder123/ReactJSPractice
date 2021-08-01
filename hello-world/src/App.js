d2bd8c1a45de9393f94c518695d791c74ffa7389
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-02 01:22:56 +0530

    State

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { Greet } from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
+import Message from './components/Message'
 
 function App() {
   return (
     <div className="App">
-      <Greet name='Ankit' heroName='h1'><p>Child prop</p></Greet>
+      <Message />
+      {/* <Greet name='Ankit' heroName='h1'><p>Child prop</p></Greet>
 
       <Greet name='Ankit2' heroName='h2'>
         <button>Action</button>
@@ -15,7 +17,7 @@ function App() {
 
       <Greet name='Ankit3' heroName='h3'></Greet>
       <Welcome name='Ankit' heroName='h1'/>
-      <Welcome name='Ankit2' heroName='h2'/>
+      <Welcome name='Ankit2' heroName='h2'/> */}
       {/* <Hello /> */}
     </div>
   );
