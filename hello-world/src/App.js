3bfda740328b0a47f0bb3399f4d04c0233c698f6
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-02 01:37:26 +0530

    11. setState

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -4,11 +4,13 @@ import { Greet } from './components/Greet'
 import Welcome from './components/Welcome'
 import Hello from './components/Hello'
 import Message from './components/Message'
+import Counter from './components/Counter'
 
 function App() {
   return (
     <div className="App">
-      <Message />
+      <Counter />
+      {/* <Message /> */}
       {/* <Greet name='Ankit' heroName='h1'><p>Child prop</p></Greet>
 
       <Greet name='Ankit2' heroName='h2'>
