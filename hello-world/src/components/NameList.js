34180019f6de842ba56e8c2abda0570bcc9ea33e
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-02 22:04:17 +0530

    17 List rendering

---
+++ hello-world/src/components/NameList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Person from './Person'
+
+function NameList() {
+    const persons = [{
+        id: 1,
+        name: 'Ankit',
+        age: 24,
+        skill: 'React'
+    },{
+        id: 2,
+        name: 'Ankit2',
+        age: 25,
+        skill: 'Angular'
+    },{
+        id: 3,
+        name: 'Ankit3',
+        age: 26,
+        skill: 'Vue'
+    }]
+    const personList = persons.map(person => <Person person={person}></Person>)
+    return <div>{personList}</div>
+}
+
+export default NameList
