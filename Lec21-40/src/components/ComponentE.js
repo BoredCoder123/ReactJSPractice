0dec51d1a7bcc5d24bcf14bddd36119be54382ce
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-05 08:04:30 +0530

    40 context part 3

--- Lec21-40/src/components/ComponentE.js
+++ Lec21-40/src/components/ComponentE.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './UserContext'
 
 class ComponentE extends Component {
+
+    static contextType = UserContext
+
     render() {
         return (
             <div>
+                Component E context {this.context}
                 <ComponentF></ComponentF>
             </div>
         )
     }
 }
+
+// ComponentE.contextType = UserContext
 
 export default ComponentE
