4a5a549ee6ada9c29e8173b282d4a5f9c69c3ab9
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-05 23:04:14 +0530

    17 useContext part 3

--- react-hooks/src/Components/ComponentE.js
+++ react-hooks/src/Components/ComponentE.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import ComponentF from './ComponentF'
+import React, {useContext} from 'react'
+import ComponentF from './ComponentF'   
+import { UserContext, ChannelContext } from '../App'
 
 function ComponentE() {
+
+    const user = useContext(UserContext)
+    const channel = useContext(ChannelContext)
+
     return (
         <div>
+            {user} {channel}
             <ComponentF></ComponentF>
         </div>
     )
