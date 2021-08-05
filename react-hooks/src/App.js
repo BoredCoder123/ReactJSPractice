0d7e8d6337a810700667024a3dd8241ec08b0ea3
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-05 19:59:30 +0530

    4- useState with object

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import HookCounter from './Components/HookCounter';
 import HookCounter2 from './Components/HookCounter2';
+import HookCounter3 from './Components/HookCounter3';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounter></HookCounter> */}
 
-      <HookCounter2></HookCounter2>
+      {/* <HookCounter2></HookCounter2> */}
+
+      <HookCounter3></HookCounter3>
     </div>
   );
 }
