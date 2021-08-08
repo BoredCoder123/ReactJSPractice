705ba95be76cedbb6e47842af398ebc2d1baf9cf
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-08 15:03:46 +0530

    Pages done

--- travel-practice/src/App.js
+++ travel-practice/src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './components/pages/Home'
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
+import Products from './components/pages/Products';
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar></Navbar>
         <Switch>
           <Route path='/' exact component={Home}/>
+          <Route path='/services' component={Services}></Route>
+          <Route path='/products' component={Products}></Route>
+          <Route path='/sign-up' component={SignUp}></Route>
         </Switch>
       </Router>
     </>
