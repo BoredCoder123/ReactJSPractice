fefd4bc86e85399ed6a9b1b0ac20962965bdfb38
Ankit Kathait <ankitkathait28@gmail.com>
2021-08-15 14:25:46 +0530

    Customising material ui components

--- material-ui-react-app/src/Components/Header.js
+++ material-ui-react-app/src/Components/Header.js
@@ -1,31 +1,61 @@
-import { AppBar, Badge, Grid, Icon, IconButton, InputBase, Toolbar } from '@material-ui/core'
+import { AppBar, Badge, Grid, Icon, IconButton, InputBase, makeStyles, Toolbar } from '@material-ui/core'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
 import PowerSettingsNewIcon from '@material-ui/icons/PowerSettingsNew';
+import SearchIcon from '@material-ui/icons/Search';
 import React from 'react'
 
+const useStyle = makeStyles({
+    root: {
+        backgroundColor: '#fff'
+    },
+    searchInput: {
+        opacity: '0.6',
+        padding: '0px 8px',
+        fontSize: '0.8rem',
+        '&:hover' :{
+            backgroundColor: '#f2f2f2'
+        },
+        '& .MuiSvgIcon-root':{
+            marginRight: '8px'
+        }
+    },
+    btnRoot:{
+        backgroundColor: 'green'
+    },
+    btnLabel:{
+        backgroundColor: 'red'
+    }
+})
+
 export default function Header() {
+
+    const classes= useStyle()
+
     return (
-        <AppBar position='static'>
+        <AppBar position='static' className={classes.root}>
             <Toolbar>
-                <Grid container>
+                <Grid container alignItems='center'>
                     <Grid item>
-                        <InputBase></InputBase>
+                        <InputBase placeholder='Search'
+                        className={classes.searchInput}
+                        startAdornment={<SearchIcon fontSize='small'/>}
+                        ></InputBase>
                     </Grid>
                     <Grid item sm></Grid>
                     <Grid item>
-                        <IconButton>
+                        <IconButton classes={{root: classes.btnRoot, label: classes.btnLabel}}>
                             <Badge badgeContent={4} color='secondary'>
-                                <NotificationsNoneIcon></NotificationsNoneIcon>
+                                <NotificationsNoneIcon fontSize='small'></NotificationsNoneIcon>
                             </Badge>
                         </IconButton>
                         <IconButton>
                             <Badge badgeContent={3} color='primary'>
-                                <ChatBubbleOutlineIcon></ChatBubbleOutlineIcon>
+                                <ChatBubbleOutlineIcon fontSize='small'></ChatBubbleOutlineIcon>
                             </Badge>
                         </IconButton>
                         <IconButton>
-                            <PowerSettingsNewIcon></PowerSettingsNewIcon>
+                            <PowerSettingsNewIcon fontSize='small'></PowerSettingsNewIcon>
                         </IconButton>
                     </Grid>
                 </Grid>
